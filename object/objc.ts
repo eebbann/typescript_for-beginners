1cab17c01cba01c0adbfd8fa64d1570386dfe19e
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-04-29 03:53:18 -0500

    type-Aliace

---
+++ object/objc.ts
@@ -0,0 +1,24 @@
+//for typesscript object follow the steps below
+
+const User ={
+	name : 'John',
+	age : 18,
+	location : 'united states',
+	isActive : false,
+} 
+
+const createUser= ({name: string, isPaid: bolean,email:{}})=>{
+  
+}
+
+createUser({name: 'John', isPaid:false, email:"enam@gmail.com"})
+
+//after following this your question might be how about a function returning an object
+//this step below helps
+
+
+const createBuyer= ():{}=>{
+  return {}
+}
+ 
+export{}
